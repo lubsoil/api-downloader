8ef34ed8dda3033eee6dc94d138e93d4ed687c14
Emil Andrzejak <emil09@op.pl>
2024-03-26 10:38:22 +0100

    Updated Test

--- api-downloader/src/test/java/pl/lubsoil/apidownloader/ApiDownloaderApplicationTests.java
+++ api-downloader/src/test/java/pl/lubsoil/apidownloader/ApiDownloaderApplicationTests.java
@@ -1,29 +1,47 @@
 package pl.lubsoil.apidownloader;
 
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import pl.lubsoil.apidownloader.model.Post;
 import pl.lubsoil.apidownloader.service.ApiDownloadService;
 
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
 
-@SpringBootTest
-class ApiDownloaderApplicationTests {
-	
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-	@Test
-	void savePost() {
-		/*File testJson = new File("C:/Users/emil0/OneDrive/Documents/Test/123.json");
+@SpringBootTest
+@Log
+class ApiDownloaderApplicationTests {
+
+	ApiDownloadService apiDownloadService;
+
+	@BeforeAll
+	static void setup() {
+		log.info("Cleaning test files");
+
+		String defaultPath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+
+		File testJson = new File(defaultPath+"/Test/123.json");
 		if(testJson.exists()){
 			testJson.delete();
 		}
+	}
 
-		List<Post> postToSave = Collections.singletonList(new Post(123,123,"test","test"));
-		apiDownloadService.savePosts(postToSave, "C:/Users/emil0/OneDrive/Documents/Test");
+	@Test
+	void saveTestPost() {
+		ApiDownloadService apiDownloadService = new ApiDownloadService();
 
-		assert( (new File("C:/Users/emil0/OneDrive/Documents/Test/123.json")).exists() );*/
+		String defaultPath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath();
+
+		List<Post> postToSave = Collections.singletonList(new Post(123,123,"title","body"));
+		apiDownloadService.savePosts(postToSave, defaultPath+"/Test/123.json");
+
+		assertTrue( (new File(defaultPath+"/Test/123.json")).exists() );
 	}
 
 }
